11f0d24d999994338f7a3c8cc963aaf19429c452
S5dG1rl <91469855+S5dG1rl@users.noreply.github.com>
2025-11-19 18:45:03 +0300

    Create dishes.js

---
+++ dishes.js
@@ -0,0 +1,16 @@
+const dishes = [
+  // Супы
+  { keyword: "chicken-noodle", name: "Куриный суп с лапшой", price: 120, category: "soup", count: "300 мл", image: "https://rugpt.io/_next/image?url=https%3A%2F%2Frugpt.hb.bizmrg.com%2Fchats%2Fe1c884cc-85ea-4745-88a8-8effc9aef296%2F488cf29b-0252-4807-bb30-14a2c5986c59.png&w=3840&q=100" },
+  { keyword: "beetroot-soup", name: "Свекольник с яйцом", price: 130, category: "soup", count: "300 мл", image: "https://rugpt.io/_next/image?url=https%3A%2F%2Frugpt.hb.bizmrg.com%2Fchats%2F4add59ff-679b-4cda-8a5b-6916aace38ed%2Fe9fe4888-7d05-429f-8713-bb6cb0255936.png&w=3840&q=100" },
+  { keyword: "mushroom-cream", name: "Грибной крем-суп", price: 150, category: "soup", count: "300 мл", image: "https://rugpt.io/_next/image?url=https%3A%2F%2Frugpt.hb.bizmrg.com%2Fchats%2F4add59ff-679b-4cda-8a5b-6916aace38ed%2F5bc70949-5a55-4f4c-9885-87d7ade6dc28.png&w=3840&q=100" },
+
+  // Главные блюда
+  { keyword: "chicken-rice", name: "Курица с овощами и рисом", price: 320, category: "main", count: "400 г", image: "https://i.pinimg.com/originals/54/f4/77/54f477a8c3d67774fd047a3076eaaccc.jpg" },
+  { keyword: "salmon-quinoa", name: "Лосось на пару с киноа", price: 450, category: "main", count: "380 г", image: "https://i.pinimg.com/originals/5d/ef/24/5def249dba97180ad98f6161f378d767.jpg" },
+  { keyword: "tofu-stew", name: "Овощное рагу с тофу", price: 290, category: "main", count: "420 г", image: "https://i.pinimg.com/originals/b9/4d/88/b94d885a577677f1f20e90da1dc1694b.jpg" },
+
+  // Напитки
+  { keyword: "green-tea", name: "Зелёный чай", price: 60, category: "drink", count: "250 мл", image: "https://avatars.mds.yandex.net/i?id=0afaa6c3d4757f1787cb5f8b60cb9c17_l-4146380-images-thumbs&n=13" },
+  { keyword: "orange-juice", name: "Свежевыжатый апельсиновый сок", price: 140, category: "drink", count: "250 мл", image: "https://media.leverans.ru/product_images_inactive/perm/belyi-slon/IMG_7428_1.jpg" },
+  { keyword: "cranberry-juice", name: "Клюквенный морс", price: 90, category: "drink", count: "250 мл", image: "https://avatars.mds.yandex.net/i?id=fad77a007fabcbd0751badebcaad1ca3_l-3514751-images-thumbs&n=13" }
+];
